b7269563da8d7823bfe92dacc021eb8a833af299
RATIKA <RATIKA>
2016-02-01 10:17:15 +0530

    First commit

---
+++ app/src/main/java/com/event/android/eventapp/fragments/About.java
@@ -0,0 +1,50 @@
+package com.event.android.eventapp.fragments;
+
+
+import android.content.Context;
+import android.os.Bundle;
+
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.event.android.eventapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class About extends Fragment {
+
+
+    public About() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
---
+++ app/src/main/java/com/event/android/eventapp/metadata/Information.java
@@ -0,0 +1,10 @@
+package com.event.android.eventapp.metadata;
+
+/**
+ * Created by jeetu on 16/2/15.
+ */
+public class Information {
+
+    public int iconId;
+    public String title;
+}

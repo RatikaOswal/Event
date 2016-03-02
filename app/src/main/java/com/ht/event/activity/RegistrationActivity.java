db409c1fa3074892d9d7b10ebad6619b65f3c13f
Ratika <ratikaratika7@gmail.com>
2016-03-02 16:19:38 +0530

    google plus login

--- app/src/main/java/com/ht/event/activity/RegistrationActivity.java
+++ app/src/main/java/com/ht/event/activity/RegistrationActivity.java
@@ -1,13 +1,35 @@
 package com.ht.event.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
 import com.ht.event.R;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -17,34 +39,67 @@ import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-public class RegistrationActivity extends AppCompatActivity  {
+import java.io.InputStream;
 
-    private CallbackManager mcallbackManager;
-    private TextView mTextDetail;
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener,
+        GoogleApiClient.OnConnectionFailedListener {
+    private static final int REQUEST_CODE_RESOLVE_ERR = 9001;
+    public CallbackManager mcallbackManager;
+    public TextView mTextDetail;
+    public SignInButton btnSignIn;
+    public ImageView imgProfilePic;
+    public TextView txtName, txtEmail, info;
+    public LinearLayout llProfileLayout;
+    public static final String TAG = "MainActivity";
+    // Profile pic image size in pixels
+    public static final int PROFILE_PIC_SIZE = 400;
+    // Google client to interact with Google API
+    public GoogleApiClient mGoogleApiClient;
+    public boolean mIntentInProgress;
+    public boolean mSignInClicked;
+    public ConnectionResult mConnectionResult;
+
 
     @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            FacebookSdk.sdkInitialize(getApplicationContext());
-            mcallbackManager = CallbackManager.Factory.create();
-            setContentView(R.layout.activity_registration);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mcallbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.activity_registration);
 
 
 //setting the toolbar
-            Toolbar mToolbar = (Toolbar) findViewById(R.id.regisbar);
-            setSupportActionBar(mToolbar);
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.regisbar);
+        setSupportActionBar(mToolbar);
 
 //setting the icon
-            getSupportActionBar().setHomeButtonEnabled(true);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-            mTextDetail=(TextView)findViewById(R.id.userinfo);
 
-            //setting loginbutton
+        btnSignIn = (SignInButton) findViewById(R.id.google_sign_in);
+        imgProfilePic = (ImageView) findViewById(R.id.imgProfilePic);
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        llProfileLayout = (LinearLayout) findViewById(R.id.llProfile);
+        info = (TextView) findViewById(R.id.info);
 
-            LoginButton logInButton=(LoginButton) findViewById(R.id.login_button);
-            logInButton.setReadPermissions("user_friends");
-            logInButton.registerCallback(mcallbackManager, new FacebookCallback<LoginResult>() {
+        // Button click listeners
+        btnSignIn.setOnClickListener(this);
+
+        //setting google+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        //setting  facebook loginButton
+        LoginButton logInButton = (LoginButton) findViewById(R.id.login_button);
+        logInButton.setReadPermissions("user_friends");
+        logInButton.registerCallback(mcallbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
 
@@ -53,13 +108,12 @@ public class RegistrationActivity extends AppCompatActivity  {
                 System.out.println("--------------------------");
                 System.out.println("User ID  : " + loginResult.getAccessToken().getUserId());
                 System.out.println("Authentication Token : " + loginResult.getAccessToken().getToken());
-                System.out.println("User Name : "+loginResult.getClass());
+                System.out.println("User Name : " + loginResult.getClass());
 
-                Profile profile=Profile.getCurrentProfile();
+                Profile profile = Profile.getCurrentProfile();
 
-                if (profile != null)
-                {
-                    mTextDetail.setText("WELCOME" +profile.getName());
+                if (profile != null) {
+                    mTextDetail.setText("WELCOME" + profile.getName());
 
                 }
             }
@@ -75,37 +129,215 @@ public class RegistrationActivity extends AppCompatActivity  {
                 System.out.println("Facebook Login failed!!");
             }
         });
+    }
 
+    protected void onStart() {
+        super.onStart();
+//        mGoogleApiClient.connect();
+    }
 
+    protected void onStop() {
+        super.onStop();
+//        if (mGoogleApiClient.isConnected()) {
+//            mGoogleApiClient.disconnect();
+//        }
+    }
+
+    /**
+     * Method to resolve any signin errors
+     */
+    private void resolveSignInError() {
+//        System.out.println("mConnectionResult.hasResolution()"+mConnectionResult.hasResolution());
+        if (mConnectionResult.hasResolution()) {
+            try {
+                mIntentInProgress = true;
+                mConnectionResult.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+            } catch (IntentSender.SendIntentException e) {
+                mIntentInProgress = false;
+                mGoogleApiClient.connect();
+            }
         }
+    }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_registration, menu);
-            return true;
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (result.hasResolution()) {
+            try {
+                result.startResolutionForResult(this, REQUEST_CODE_RESOLVE_ERR);
+            } catch (IntentSender.SendIntentException e) {
+                mGoogleApiClient.connect();
+            }
         }
+        // Save the result and resolve the connection failure upon a user click.
+        mConnectionResult = result;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_registration, menu);
+        return true;
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("onActivityResult"+requestCode+"resultCode"+resultCode);
         mcallbackManager.onActivityResult(requestCode, resultCode, data);
-
-//        twitterLogin.onActivityResult(requestCode, resultCode, data);
-    }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            int id = item.getItemId();
-
-            if (id == android.R.id.home) {
-                onBackPressed();
-                return true;
+        if (requestCode == REQUEST_CODE_RESOLVE_ERR) {
+            if (resultCode != RESULT_OK) {
+                mSignInClicked = false;
+            }
+            else{
+                GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+                getProfileInformation(result);
             }
 
-            return super.onOptionsItemSelected(item);
+            mIntentInProgress = false;
+
+            if (!mGoogleApiClient.isConnecting()) {
+                mGoogleApiClient.connect();
+            }
         }
 
-
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+//    @Override
+//    public void onConnected(Bundle bundle) {
+//        mSignInClicked = false;
+//        Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+//
+//        // Get user's information
+////        getProfileInformation();
+//
+//        // Update the UI after signin
+//        updateUI(true);
+//
+//
+//    }
+
+    /**
+     * Updating the UI, showing/hiding buttons and profile layout
+     */
+    private void updateUI(boolean isSignedIn) {
+        if (isSignedIn) {
+            btnSignIn.setVisibility(View.GONE);
+            info.setVisibility(View.GONE);
+            llProfileLayout.setVisibility(View.VISIBLE);
+        } else {
+            btnSignIn.setVisibility(View.VISIBLE);
+            info.setVisibility(View.VISIBLE);
+            llProfileLayout.setVisibility(View.GONE);
+        }
+    }
+
+    /**
+     * Fetching user's information name, email, profile pic
+     */
+    private void getProfileInformation(GoogleSignInResult result) {
+        try {
+            if (result.isSuccess()){
+                String personName = result.getSignInAccount().getDisplayName();
+                String personPhotoUrl =  result.getSignInAccount().getPhotoUrl().toString();
+                String email = result.getSignInAccount().getEmail();
+
+                Log.e(TAG, "Name: " + personName + ", plusProfile: "
+                         + ", email: " + email
+                        + ", Image: " + result.getSignInAccount().getPhotoUrl());
+
+                txtName.setText(personName);
+                txtEmail.setText(email);
+                // by default the profile url gives 50x50 px image only
+                // we can replace the value with whatever dimension we want by
+                // replacing sz=X
+//                personPhotoUrl = personPhotoUrl.substring(0,
+//                        personPhotoUrl.length() - 2)
+//                        + PROFILE_PIC_SIZE;
+                if(personPhotoUrl != null) {
+                    new LoadProfileImage(imgProfilePic).execute(personPhotoUrl);
+                }
+
+                updateUI(true);
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        "Person information is null", Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+//    @Override
+//    public void onConnectionSuspended(int arg) {
+//        mGoogleApiClient.connect();
+//        updateUI(false);
+//
+//    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.google_sign_in:
+                // SignIn button clicked
+                signInWithGplus();
+                break;
+
+        }
+    }
+
+    private void signInWithGplus() {
+        System.out.println("signInWithGplus called");
+         if(!mGoogleApiClient.isConnected()){
+            System.out.println("mGoogleApiClient connected");
+            mGoogleApiClient.connect();
+        }
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, REQUEST_CODE_RESOLVE_ERR);
+
+        }
+
+    /**
+     * Background Async task to load user profile picture from url
+     * */
+    private class LoadProfileImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public LoadProfileImage(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+
+}

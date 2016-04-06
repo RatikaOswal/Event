1194ab9a8bb4e202049b4c4863c3f96bae37abad
komal <negiriya26@gmail.com>
2016-04-06 09:48:59 +0530

    filter

--- app/src/main/java/com/ht/event/activity/FilterActivity.java
+++ app/src/main/java/com/ht/event/activity/FilterActivity.java
@@ -1,9 +1,11 @@
 package com.ht.event.activity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
@@ -16,6 +18,8 @@ import android.widget.TextView;
 import com.ht.event.adapter.listAdapter;
 
 import com.ht.event.R;
+import com.ht.event.model.Event;
+import com.ht.event.utils.Config;
 
 
 import java.util.ArrayList;
@@ -34,6 +38,7 @@ public class FilterActivity extends AppCompatActivity {
     private RelativeLayout checkbox_relativeLayout;
     private TextView event_types;
     private RelativeLayout event_types_relativelayout;
+    private Event event;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,16 +51,16 @@ public class FilterActivity extends AppCompatActivity {
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        event = (Event)getIntent().getSerializableExtra(Config.ITEM_INTENT_OBJECT);
         checkbox_relativeLayout =(RelativeLayout)findViewById(R.id.checkbox_relativelayout);
         datesradiogroup = (RadioGroup)findViewById(R.id.dates_radioGroup);
         event_topics =(TextView)findViewById(R.id.event_topics);
         event_topics.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(checkbox_relativeLayout.getVisibility()==View.VISIBLE) {
+                if (checkbox_relativeLayout.getVisibility() == View.VISIBLE) {
                     checkbox_relativeLayout.setVisibility(View.GONE);
-                }
-                else {
+                } else {
                     checkbox_relativeLayout.setVisibility(View.VISIBLE);
                 }
 
@@ -68,15 +73,12 @@ public class FilterActivity extends AppCompatActivity {
             public void onClick(View v) {
 
 
-                if(datesradiogroup.getVisibility()==View.VISIBLE) {
+                if (datesradiogroup.getVisibility() == View.VISIBLE) {
                     datesradiogroup.setVisibility(View.GONE);
 
-                }
-                else {
+                } else {
                     datesradiogroup.setVisibility(View.VISIBLE);
                 }
-
-
 
 
             }
@@ -86,11 +88,10 @@ public class FilterActivity extends AppCompatActivity {
         event_types.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(event_types_relativelayout.getVisibility()==View.VISIBLE) {
+                if (event_types_relativelayout.getVisibility() == View.VISIBLE) {
                     event_types_relativelayout.setVisibility(View.GONE);
 
-                }
-                else {
+                } else {
                     event_types_relativelayout.setVisibility(View.VISIBLE);
                 }
 
@@ -144,17 +145,19 @@ public class FilterActivity extends AppCompatActivity {
         EventTypes.add("Seminar");
         EventTypes.add("Expo");
         EventTypes.add("Festival");
-        EventTypes.add("Perforrmance");
+        EventTypes.add("Performance");
 
         listDataChild.put(listDataHeader.get(0), Dates); // Header, Child data
         listDataChild.put(listDataHeader.get(1), EventTopics);
         listDataChild.put(listDataHeader.get(2), EventTypes);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_filter, menu);
 
-
-
-
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -163,6 +166,12 @@ public class FilterActivity extends AppCompatActivity {
         if (id == android.R.id.home) {
             onBackPressed();
             return true;
+        }
+
+        if(id==R.id.filterApply){
+            Intent intent = new Intent(this,MainActivity.class);
+            intent.putExtra(Config.ITEM_INTENT_OBJECT,event);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
